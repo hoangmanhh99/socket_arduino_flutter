713683620f7f8503317bb923e34b73f989539110
Hoang Van Manh <hoangmanhh99@gmail.com>
2022-06-06 11:33:10 +0700

    Update port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-var port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -23,6 +23,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log('listening on *:3000');
 });
