6a9dde40167125b5b8f54db6f7ed57c6c95ba0a3
Hoang Van Manh <hoangmanhh99@gmail.com>
2022-06-06 11:15:11 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var port = process.env.port || 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -21,6 +23,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
